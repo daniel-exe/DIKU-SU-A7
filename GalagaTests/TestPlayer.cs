75a7d0503b1c43690c43300998ae363d9a658033
Daniel Mølhave <dmolhave@gmail.com>
2024-03-11 19:11:01 +0100

    More tests and style change in player.cs

--- GalagaTests/TestPlayer.cs
+++ GalagaTests/TestPlayer.cs
@@ -16,20 +16,50 @@ namespace GalagaTests;
 
 public class TestsPlayer {
 
-    public Player player;
-
     [SetUp]
-    public void Setup() {
-        player = new Player(
-            new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
-            new NoImage());
+    public void Setup() {}
+
+    private Player player = new Player(
+        new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
+        new NoImage()
+    );
+
+    [Test]
+    public void TestMoveLeft() {
+        // Arrange
+        player.SetMoveRight(true);
+        player.Move();
+        player.SetMoveRight(false);
+        float oldX = player.GetPosition().X;
+        // Act
+        player.SetMoveLeft(true);
+        player.Move();
+        player.SetMoveLeft(false);
+        // Assert
+        Assert.True(player.GetPosition().X == (oldX - 0.01f));
     }
 
     [Test]
-    public void Test1() {
+    public void TestMoveRight() {
         float oldX = player.GetPosition().X;
         player.SetMoveRight(true);
+
         player.Move();
-        Assert.True(player.GetPosition().X == (oldX + 0.1f));
+        player.SetMoveRight(false);
+
+        Assert.True(player.GetPosition().X == (oldX + 0.01f));
+    }
+
+    [Test]
+    public void TestMoveBoundary() {
+        player.SetMoveLeft(true);
+        while (player.GetPosition().X > 0.0f) {
+            player.Move();
+        }
+
+        player.Move();
+        player.SetMoveLeft(false);
+
+        Assert.True(player.GetPosition().X == 0.0f);
     }
 }
